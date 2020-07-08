ae0010f9afb4d22d101b2b086d04649a436618e6
Kanishq <kanishq@spennyapp.com>
2020-07-09 03:03:45 +0530

    changes

--- express/server.js
+++ express/server.js
@@ -1,22 +1,75 @@
-'use strict';
 const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
+const path = require('path');
+const { google } = require('googleapis');
+const keys = require('../key.json');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+app.use(express.static('./client/build'));
+app.use(express.json());
+app.use(cors());
+
+var sheet = google.sheets('v4');
+
+const port = process.env.PORT || 5003;
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+let id;
+app.post('/register', (req, res) => {
+  const { name, email, phone, class1, gender, subject } = req.body;
 
-module.exports = app;
-module.exports.handler = serverless(app);
+  //   const spreadsheet = {
+  //     sheet1: {
+  //       id: '1s9_umcUaSxCHzY_AfFz3LAV-DbFVpBouk4tNU8YxxR4',
+  //     },
+  //   };
+  //   for (let sheets in spreadsheet) {
+  //     value = spreadsheet[sheets];
+  //     let eventarr = value.events;
+  //     let b = eventarr.some((item) => events.includes(item));
+  //     id = value.id;
+  //     if (b == true) {
+  //       console.log(id);
+  //       authorize(add);
+  //     }
+  //   }
+  authorize(add);
+
+  async function add(authClient) {
+    let cur_date = Date();
+    var participant = {
+      spreadsheetId: '1mszc_xSrj0G7193je-LhAjkNcO8JkJCNgPkYwee17fY',
+      range: 'A1',
+      auth: authClient,
+      valueInputOption: 'RAW',
+      insertDataOption: 'INSERT_ROWS',
+      resource: {
+        values: [[name, email, phone, class1, gender, cur_date, subject]],
+      },
+    };
+
+    sheet.spreadsheets.values.append(participant, function (err, response) {
+      if (err) {
+        console.error(err);
+        res.status(500).send();
+      }
+      res.status(200).send();
+    });
+  }
+});
+
+async function authorize(callback) {
+  var authClient = new google.auth.JWT(keys.client_email, null, keys.private_key, [
+    'https://www.googleapis.com/auth/spreadsheets',
+  ]);
+  if (authClient == null) {
+    console.log('authentication failed');
+    return;
+  }
+  callback(authClient);
+}
+
+app.listen(port, () => console.log(`listening on port ${port}`));
